0c26cf4de31b03ee392a9defee200f36353ac59b
Johan Tique <johan.tique@codescrum.com>
2014-01-27 18:26:39 -0500

    add some gobals variables in rsv::params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,11 @@
-class replica-set-verifier::params {
+class rsv::params {
+	# main instalation directory 
 	$main_directory = '/opt/replica_set_verifier'
+	# main repo of LIBBSON
+	$libbson_repo = 'https://github.com/mongodb/libbson/releases/download/0.4.0/libbson-0.4.0.tar.gz'
+	# main repo of MONGO-C-Driver
+	$libmongo_repo = 'https://github.com/mongodb/mongo-c-driver/archive/v0.8.1.tar.gz'
+	# main repo of replica_set_verifier
+	$rsv_repo = 'https://github.com/Johaned/replica_set_verifier/archive/0.1.0.tar.gz'
+
 }
