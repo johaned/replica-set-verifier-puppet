399d71b797af524d8184269fefebda0339616a95
Johan Tique <johan.tique@codescrum.com>
2014-01-27 18:28:42 -0500

    add class to install replica_set_verifier package

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,20 @@
-# == Class: replica-set-verifier::install
+# == Class: rsv::install
 #
 #
-class replica-set-verifier::install {
-	anchor { 'replica-set-verifier::install::begin': }
-	anchor { 'replica-set-verifier::install::end': }
+class rsv::install {
+    include rsv::dependencies
+
+	anchor { 'rsv::install::begin':
+	  before => Anchor['rsv::dependencies::begin'],
+	}
+
+    anchor { 'rsv::install::end': }
+
+	netinstall { 'rsv':
+	  url => $::rsv::params::rsv_repo,
+	  extracted_dir => 'replica_set_verifier-0.1.0',
+	  destination_dir => '/tmp',
+	  postextract_command => 'make && sudo make install',
+      require => Anchor['rsv::dependencies::end']
+	}
 }
